5d6a9f1728bf4c6f645666d1d4327150a1a2183f
Alex Saxton <alex.saxton98@gmail.com>
2021-10-14 12:19:32 +0100

    added create and read functions

--- app.js
+++ app.js
@@ -3,4 +3,56 @@ const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
 
-console.log(argv)
+const mongoose = require("mongoose");
+// const { string } = require("yargs");
+
+mongoose.connect("mongodb://localhost:27017/friends_list",);
+
+const Friend = mongoose.model("Friend",
+{
+    name : String
+})
+
+const add = async (name) => {
+    const friend = new Friend({name: name})
+    console.log(friend)
+    await friend.save();
+    process.exit();
+}
+
+const findOne = async (name) => {
+    const friend = await Friend.findOne({name: name})
+    console.log(friend)
+    process.exit()
+}
+
+const findAll = async () => {
+    const friend = await Friend.find()
+    console.log(friend)
+    process.exit()
+}
+
+const findSome = async (name) => {
+    const friend = await Friend.find({name: name})
+    console.log(friend)
+    process.exit()
+}
+
+const main = async (argv) => {
+    try {
+        if(argv.add) {
+            await add(argv.name)
+        } else if (argv.findOne) {
+            await findOne(argv.name)
+        } else if (argv.findAll) {
+            await findAll()
+        } else if (argv.findSome) {
+            await findSome(argv.name)
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+main(argv)
+// console.log(argv)
